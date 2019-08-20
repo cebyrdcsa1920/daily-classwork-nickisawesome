e6381003645c0d32906b2bb43d7746e601bacca1
nickisawesome <nicholaslmitchell@icloud.com>
2019-08-20 14:14:48 +0000

    Copy over files from Monday

---
+++ HelloName.java
@@ -0,0 +1,8 @@
+public class HelloName
+{
+     public static void main(String[] args)
+    {
+        String name = "Nickisawesome";
+        System.out.println("Hello," + name)
+    }
+}
---
+++ Nickisawesome.java
@@ -0,0 +1,7 @@
+public class Nickisawesome
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello, it's been a while Nick :)");
+    }
+}

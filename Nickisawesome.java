9839040a653c95f64b13df984afe6dbcaf0f5230
nickisawesome <nicholaslmitchell@icloud.com>
2019-09-04 13:48:28 +0000

    Create Car.java and its no-args constructor

--- Nickisawesome.java
+++
@@ -1,7 +0,0 @@
-public class Nickisawesome
-{
-    public static void main(String[] args)
-    {
-        System.out.println("hello, it's been a while Nick :)");
-    }
-}
---
+++ week2/Student.java
@@ -0,0 +1,21 @@
+public class Student
+{
+    private double gpa;
+    private String studentID;
+    private String name;
+    private int age;
+
+    public Student(String name, String studentID, int age)
+    {
+        this.gpa = 0.0;
+        this.name = name;
+        this.studentID = studentID;
+        this.age = age;
+    }
+
+    public static void main(String[] args)
+    {
+        Student Nick = new Student("Nick", "672214", 16);
+        System.out.println(Nick);
+    }
+}
